de52d5f76e1786c953e536b5348c301e935d00e4
bukrathezer <104260118+bukrathezer@users.noreply.github.com>
2022-08-22 08:41:59 +0300

    web

---
+++ login1.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>SAKARYA ÜNİVERSİTESİ PROJE</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+	   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.slim.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+      <link rel="stylesheet" href="tasarim.css">
+	  <link rel='stylesheet' href='logindogrulama.css' type='text/css' />
+		<script src="logindogrulama.js"></script>
+  
+</head>
+    
+<body onload="document.iletisimformu.kadi.focus();">
+  <div class="container">
+    <div class="orta">
+	<br>	
+HOŞGELDİNİZ <b style="color:#d29583;"><?php echo $_POST["email"]; ?></b><br>
+
+        <div class="temizle"></div>
+		
+    </div>
+	<br>
+</div>       
+</body>
+</html>
